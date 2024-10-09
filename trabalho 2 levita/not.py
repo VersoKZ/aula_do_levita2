ee962bffe5ad7a186747f222cecca2e44f79821d
Pedro Santana <123907570+VersoKZ@users.noreply.github.com>
2024-10-09 12:57:28 -0300

    Update not.py

--- trabalho 2 levita/not.py
+++ trabalho 2 levita/not.py
@@ -1,1 +1,20 @@
+quantidade = 0
+soma = 0
+media = 0
+
+valor = float(input('digite a nota: '))
+
+if valor >= 0 and valor < 11:
+    perg = input('deseja continuar? (s/n): ')
+    while perg == 's':
+        soma = soma + valor
+        quantidade = quantidade + 1
+        valor = float(input('digite a nota: '))
+        perg = input('deseja continuar? (s/n): ')
+
+    res = soma / quantidade
+    print(range(res, 2))
+
+else:
+    print('valor é invalido')
 
